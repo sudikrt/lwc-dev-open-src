7d3278a7407c02400aef036076c9485a0dd19dd3
sudikrt <sudarshankaranth18@gmail.com>
2019-07-25 19:57:15 +0530

    fetch fun

--- src/modules/my/button/button.js
+++ src/modules/my/button/button.js
@@ -1,11 +1,22 @@
-import SldsElement from "utils/sldsElement";
-import {api} from "lwc";
+import SldsElement from 'utils/sldsElement';
+import { api } from 'lwc';
 
 export default class Button extends SldsElement {
     @api message;
 
-    getData () {
+    getData() {
         // eslint-disable-next-line no-alert
-        alert (this.message);
+        //alert(this.message);
+
+        fetch (`http://localhost:5000/upper?msg=${this.message}`)
+        .then (response => {return response.text ()})
+        .then (text => {
+            // eslint-disable-next-line no-alert
+            alert(text);
+        })
+        .catch (error => {
+            // eslint-disable-next-line no-alert
+            alert(`Failed: ${error}`);
+        })
     }
 }
--- src/modules/utils/sldsElement/sldsElement.js
+++ src/modules/utils/sldsElement/sldsElement.js
@@ -1,14 +1,13 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
 
 export default class SldsElement extends LightningElement {
-    constructor () {
-        super ();
+    constructor() {
+        super();
         let _sldsPath = '../SLDS/styles';
         _sldsPath += '/salesforce-lightning-design-system.css';
-        const styles = document.createElement ('link');
+        const styles = document.createElement('link');
         styles.href = _sldsPath;
         styles.rel = 'stylesheet';
-        this.template.appendChild (styles);
-
+        this.template.appendChild(styles);
     }
 }
